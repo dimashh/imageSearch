f87fba6642f466a26dd34ed3bdcf6ecb29a7e4b5
“dimashh” <dimash97@hotmail.co.uk>
2019-10-24 21:23:20 +0600

    Query styling.

--- src/ImageHolderComponent.js
+++ src/ImageHolderComponent.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 class ImageHolderComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { rows: 0 }
+    this.imageReference = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageReference.current.addEventListener('load', this.setRows);
+  }
+
+  setRows = () => {
+    const height = this.imageReference.current.clientHeight;
+    const rows = Math.ceil(height/10);
+    this.setState({ rows: rows });
+    console.log(this.imageReference.current.clientHeight);
+  }
+
   render() {
     return (
-      <div>
-          <img src={this.props.image.urls.regular}
-               alt={this.props.image.alt_description} />
+      <div style={{ gridRowEnd: `span ${this.state.rows}`}}>
+          <img ref={this.imageReference}
+            src={this.props.image.urls.regular}
+            alt={this.props.image.alt_description} />
       </div>
     )
   }
